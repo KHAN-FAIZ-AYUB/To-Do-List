298fc8b0836a49d104cca3006349e4078adbb8c2
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-11-22 20:32:51 +0530

    adding date

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const btnAdd = document.getElementById("add-btn");
 var items = [];
 btnAdd.addEventListener("click", function () {
+  const date = document.getElementById("date").value;
   const itemEnterd = document.getElementById("textbox").value;
   document.getElementById("textbox").value = "";
   if (itemEnterd == "") {
     alert("please enter task");
     return false;
   } else {
-    items.push(itemEnterd);
-    displayArray(itemEnterd);
+    items.push(itemEnterd, date);
+    displayArray(itemEnterd, date);
   }
+  // console.log(date);
 });
 
 function displayArray(element) {
@@ -18,16 +20,21 @@ function displayArray(element) {
 
   const li = document.createElement("li");
   li.style.border = "1px solid black";
-  li.style.width = "200px";
+  li.style.width = "250px";
   li.style.display = "flex";
   li.style.justifyContent = "space-between";
+  li.style.alignItems = "center";
   li.style.marginBottom = "5px";
 
   let button = document.createElement("button");
   let paragraph = document.createElement("p");
+  let dateBox = document.createElement("date");
 
   paragraph.textContent = element;
   li.appendChild(paragraph);
+
+  dateBox.textContent = date.value;
+  li.appendChild(dateBox);
 
   const text = document.createTextNode("Delete");
   button.appendChild(text);
