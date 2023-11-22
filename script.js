75451ce84e822fb85b614badc6acc6eb0312608c
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-11-22 18:03:55 +0530

    initial

--- script.js
+++ script.js
@@ -2,15 +2,28 @@ const btnAdd = document.getElementById("add-btn");
 var items = [];
 btnAdd.addEventListener("click", function () {
   const itemEnterd = document.getElementById("textbox").value;
-  items.push(itemEnterd);
+  items.push({ value: itemEnterd, id: new Date().getTime() });
   console.log(itemEnterd);
   displayArray(itemEnterd);
 });
 
 function displayArray(element) {
   const displayElements = document.getElementById("displayelements");
+  const li = document.createElement("li");
+  // li.style.display = "inline";
 
+  let button = document.createElement("button");
   let paragraph = document.createElement("p");
+
   paragraph.textContent = element;
-  displayElements.appendChild(paragraph);
+  li.appendChild(paragraph);
+
+  let text = document.createTextNode("Delete");
+  button.appendChild(text);
+  li.appendChild(button);
+  displayElements.appendChild(li);
+
+  button.addEventListener("click", function () {
+    li.remove();
+  });
 }
