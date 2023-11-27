cf8a4fa43d314e84302e4f9c526f0a105ceef66e
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-11-27 17:35:41 +0530

    initial

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btnAdd = document.getElementById("add-btn");
-const toDo = document.getElementById("to-do-list");
+// const toDo = document.getElementById("to-do-list");
+
 var items = [];
 btnAdd.addEventListener("click", function () {
   const date = document.getElementById("date").value;
@@ -9,47 +10,60 @@ btnAdd.addEventListener("click", function () {
     alert("please enter task");
     return false;
   } else {
-    items.push(itemEnterd, date);
-    displayArray(itemEnterd, date);
+    items.push({ task: itemEnterd, date, id: new Date() });
+    displayArray();
+    Array.from(items)
+    console.log(items);
+
   }
 });
 
-function displayArray(element) {
+function displayArray() {
   const displayElements = document.getElementById("displayelements");
-  const div = document.createElement("div");
+  items.forEach((element) => {
+    console.log(element);
+    const div = document.createElement("div");
+    const li = document.createElement("li");
+    li.style.border = "1px solid black";
+    li.style.width = "300px";
+    li.style.display = "flex";
+    li.style.justifyContent = "space-between";
+    li.style.alignItems = "center";
+    li.style.marginBottom = "5px";
+    li.style.padding = "5px";
 
-  const li = document.createElement("li");
-  li.style.border = "1px solid black";
-  li.style.width = "300px";
-  li.style.display = "flex";
-  li.style.justifyContent = "space-between";
-  li.style.alignItems = "center";
-  li.style.marginBottom = "5px";
-  li.style.padding = "5px";
+    let button = document.createElement("button");
+    let paragraph = document.createElement("p");
+    let dateBox = document.createElement("date");
+    let completeButton = document.createElement("button");
 
-  let button = document.createElement("button");
-  let paragraph = document.createElement("p");
-  let dateBox = document.createElement("date");
-  let completeButton = document.createElement("button");
+    paragraph.textContent = element;
+    li.appendChild(paragraph);
 
-  paragraph.textContent = element;
-  li.appendChild(paragraph);
+    dateBox.textContent = date.value;
+    li.appendChild(dateBox);
 
-  dateBox.textContent = date.value;
-  li.appendChild(dateBox);
+    const del = document.createTextNode("Delete");
+    button.appendChild(del);
+    li.appendChild(button);
 
-  const del = document.createTextNode("Delete");
-  button.appendChild(del);
-  li.appendChild(button);
+    const complete = document.createTextNode("Complete");
+    completeButton.appendChild(complete);
+    li.appendChild(completeButton);
 
-  const complete = document.createTextNode("Complete");
-  completeButton.appendChild(complete);
-  li.appendChild(completeButton);
+    div.appendChild(li);
+    displayElements.appendChild(li);
 
-  div.appendChild(li);
-  displayElements.appendChild(li);
-
-  button.addEventListener("click", function () {
-    li.remove();
+    button.addEventListener("click", function () {
+      li.remove();
+    });
   });
 }
+
+// -----------------------------------------------------------
+
+// let arr = ["s", "b", "f", "t", "q", "h", "l"];
+// console.log(arr);
+// console.log(arr.slice(2, 5));
+
+// console.log(arr.splice(2, 3));
